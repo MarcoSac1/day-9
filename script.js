6ce4a7e121695b74c066cbd89ad765cb7883ce10
MarcoSac1 <marcosaccone8@gmail.com>
2025-01-23 16:31:05 +0100

    completed exercise

--- script.js
+++ script.js
@@ -5,13 +5,40 @@ paragraph.innerHTML=('ciao sono un paragrafo')
 
 // Crea una lista con alcuni elementi <li> e un pulsante che, quando cliccato, cambia il colore del testo di tutti gli elementi della lista.
 
-// console.log('liEl');
 const btnEl = document.querySelector('button')
 const liEl = document.querySelectorAll('li');
+// console.log('liEl');
 
 btnEl.addEventListener('click', function(){
     liEl.forEach(function (liEl) {
         liEl.classList.add("red");
         })
     console.log('btnEl');
-})
+});
+
+
+const sendBtn = document.querySelector('form > button');
+// console.log('sendBtn');
+const ulEl = document.querySelector('ul');
+// console.log('ulEL');
+
+
+sendBtn.addEventListener('click', function(event){
+    
+    let inpT = document.querySelector('input'); // seleziono l'input 
+    
+    if (inpT.value == 0) {
+        alert('first text inside input');
+    
+    }else{
+        event.preventDefault(); // Previene il comportamento predefinito del pulsante
+    
+        let newLi = document.createElement('li'); // Crea un nuovo elemento li
+    
+        newLi.textContent = inpT.value; // Imposta il contenuto del li con il valore dell'input
+    
+        ulEl.appendChild(newLi); // Aggiungi il nuovo li alla lista ul
+    
+        inpT.value = ''; // Resetta il valore dell'input
+    }
+});
